f88927994b5f0123ba66fa1d580f89b066a8c02c
LinuxPlus <linuxplus@192.168.47.128>
2018-02-14 00:34:29 +0300

    graph view as line

--- DrinkWater/DrinkWater/GraphView.swift
+++ DrinkWater/DrinkWater/GraphView.swift
@@ -10,12 +10,29 @@ import UIKit
 
 @IBDesignable class GraphView: UIView {
 
+	var graphPoints = [4, 2, 6, 4, 5, 8, 3]
+	
+	private struct Constants {
+		static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
+		static let margin: CGFloat = 20.0
+		static let topBorder: CGFloat = 60
+		static let bottomBorder: CGFloat = 50
+		static let colorAlpha: CGFloat = 0.3
+		static let circleDiameter: CGFloat = 5.0
+	}
+	
 	@IBInspectable var startColor: UIColor = .red
 	@IBInspectable var endColor: UIColor = .green
 	
 	override func draw(_ rect: CGRect) {
+		let width = rect.width
+		let height = rect.height
 		
-        let context = UIGraphicsGetCurrentContext()!
+		let path = UIBezierPath(roundedRect: rect,
+		                        byRoundingCorners: .allCorners,
+		                        cornerRadii: Constants.cornerRadiusSize)
+		path.addClip()
+		let context = UIGraphicsGetCurrentContext()!
 		let colors = [startColor.cgColor, endColor.cgColor]
 		
 		let colorSpace = CGColorSpaceCreateDeviceRGB()
@@ -28,7 +45,73 @@ import UIKit
 		let startPoint = CGPoint.zero
 		let endPoint = CGPoint(x: 0, y: bounds.height)
 		context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
+		
+		let margin = Constants.margin
+		let graphWidth = width - margin * 2 - 4
+		let columnXPoint = {
+			(column: Int) -> CGFloat in
+			let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
+			return CGFloat(column) * spacing + margin + CGFloat(2)
+		}
+		let topBorder = Constants.topBorder
+		let bottomBorder = Constants.bottomBorder
+		let graphHeight = height - topBorder - bottomBorder
+		let maxValue = graphPoints.max()!
+		let columnYPoint = { (graphPoint: Int) -> CGFloat in
+			let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+			return graphHeight + topBorder - y
+		}
+		
+		UIColor.white.setFill()
+		UIColor.white.setStroke()
+		
+		let graphPath = UIBezierPath()
+		
+		graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+		
+		for i in 1..<graphPoints.count {
+			let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+			graphPath.addLine(to: nextPoint)
+		}
+		
+		//graphPath.stroke()
+		
+		let clippingPath = graphPath.copy() as! UIBezierPath
+		
+		clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y: height))
+		clippingPath.addLine(to: CGPoint(x: columnXPoint(0), y: height))
+		clippingPath.close()
+		clippingPath.addClip()
+		
+		let highestYPoint = columnYPoint(maxValue)
+		let graphStartPoint = CGPoint(x: margin, y: highestYPoint)
+		let graphEndPoint = CGPoint(x: margin, y: bounds.height)
+		
+		// draw the line on top of the clipped gradient
+		context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
+		graphPath.lineWidth = 2.0
+		graphPath.stroke()
+
     }
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
