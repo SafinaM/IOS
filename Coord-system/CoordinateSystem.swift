3a1d96b09236f755c2761f68f403ba2b61c2810f
LinuxPlus <>
2018-03-25 20:28:28 +0300

    circle test drawing

--- Coord-system/CoordinateSystem.swift
+++ Coord-system/CoordinateSystem.swift
@@ -14,9 +14,9 @@ class CoordinateSystem: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-		let path = UIBezierPath(ovalIn: rect)
-		path.move(to: CGPoint(x: 240, y: 240))
-//		path.addArc(withCenter: CGPoint(x: 200, y: 200) , radius: 40, startAngle: 0, endAngle: 360, clockwise: true)
+		let path = UIBezierPath()
+//		path.move(to: CGPoint(x: 240, y: 240))
+		path.addArc(withCenter: CGPoint(x: 200, y: 200) , radius: 40, startAngle: 0, endAngle: 360, clockwise: true)
 		path.stroke()
     }
 }
