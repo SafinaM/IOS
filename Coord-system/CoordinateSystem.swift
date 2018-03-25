2556d5d9a34bbc3ab4551b6ceae4a0cebd85d8dd
LinuxPlus <>
2018-03-26 00:15:32 +0300

    rotation matrix with z axis

--- Coord-system/CoordinateSystem.swift
+++ Coord-system/CoordinateSystem.swift
@@ -16,6 +16,9 @@ class CoordinateSystem: UIView
 		static let origin2 = CGPoint(x: 200, y: 400)
 		static let origin3 = CGPoint(x: 200, y: 600)
 		static let pointArray: [Point] = [Point(0, -200, 0), Point(200, 0, 0), Point(0, 0, 200)]
+		static let pitch: CGFloat = 0.2
+		static let yaw: CGFloat = -0.6
+		static let roll: CGFloat = 0.1
 	}
 	
 	struct Point {
@@ -23,7 +26,7 @@ class CoordinateSystem: UIView
 		var y: CGFloat = 0
 		var z: CGFloat = 0
 		
-		init(_ x: CGFloat, _ y: CGFloat, _ z: CGFloat) {
+		init(_ x: CGFloat = 0, _ y: CGFloat = 0, _ z: CGFloat = 0) {
 			self.x = x
 			self.y = y
 			self.z = z
@@ -33,6 +36,36 @@ class CoordinateSystem: UIView
 			return CGPoint(x: x, y: y)
 		}
 	}
+	
+	private func rotatePoint(point: Point, pitch: CGFloat, yaw: CGFloat, roll: CGFloat) -> Point {
+		
+		var pointRoll = Point()
+		pointRoll.x = point.x * cos(roll) - point.y * sin(roll)
+		pointRoll.y = point.x * sin(roll) + point.y * cos(roll)
+		pointRoll.z = point.z
+		
+		var pointYaw = Point()
+		pointYaw.x = pointRoll.x * cos(yaw) + pointRoll.z * sin(yaw)
+		pointYaw.y = pointRoll.y
+		pointYaw.z = -pointRoll.x * sin(yaw) + pointRoll.z * cos(yaw)
+		
+		var pointPitch = Point()
+		pointPitch.x = pointYaw.x
+		pointPitch.y = pointYaw.y * cos(pitch) - pointYaw.z * sin(pitch)
+		pointPitch.z = pointYaw.y * sin(pitch) + pointYaw.z * cos(pitch)
+		
+		return pointPitch
+	}
+	
+	private func rotatePointArray(points: [Point], pitch: CGFloat, yaw: CGFloat, roll: CGFloat) -> [Point] {
+		var rotatedPoints = [Point]()
+		for p in points {
+			let rotatedPoint = rotatePoint(point: p, pitch: pitch, yaw: yaw, roll: roll)
+			rotatedPoints.append(rotatedPoint)
+		}
+		return rotatedPoints
+	}
+	
 	private func moveToOrigin(point: CGPoint, origin: CGPoint) -> CGPoint {
 		let newPoint: CGPoint = CGPoint(x: point.x + origin.x, y: point.y + origin.y)
 		return newPoint
@@ -52,12 +85,12 @@ class CoordinateSystem: UIView
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
 		let path = UIBezierPath()
-		let array = moveArrayToOrigin(points: Constants.pointArray, origin: Constants.origin3)
+		let rotatedArray = rotatePointArray(points: Constants.pointArray, pitch: Constants.pitch, yaw: Constants.yaw, roll: Constants.roll)
+		let array = moveArrayToOrigin(points: rotatedArray, origin: Constants.origin3)
 		for p in array {
 			path.move(to: Constants.origin3)
 			path.addLine(to: p)
 		}
-//		path.addArc(withCenter: CGPoint(x: 200, y: 200) , radius: 40, startAngle: 0, endAngle: 360, clockwise: true)
 		UIColor.green.set()
 		path.stroke()
     }
